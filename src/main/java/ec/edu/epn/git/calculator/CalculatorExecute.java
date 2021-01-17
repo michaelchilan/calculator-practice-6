66dac0a07e4a59e049ece8b9060b6bda860299ae
michaelchilan <michael.chilan@epn.edu.ec>
2021-01-16 23:11:23 -0500

    Input of New Operations in class CalculatorExecute.

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -16,5 +16,19 @@ public class CalculatorExecute {
 
         double division = c.division(500, 5);
         System.out.println("c.division(500,5) = " + division);
+
+        System.out.println("== Input of New Operations ==");
+
+        int addition1 = c.addition(200, 700);
+        System.out.println("c.addition(200,700) = " + addition1);
+
+        int subtraction1 = c.subtraction(900, 698);
+        System.out.println("c.subtraction(900,698) = " + subtraction1);
+
+        int multiply1 = c.multiply(500, 834);
+        System.out.println("c.multiply(500,834) = " + multiply1);
+
+        double division1 = c.division(5000, 575);
+        System.out.println("c.division(5000,575) = " + division1);
     }
 }
